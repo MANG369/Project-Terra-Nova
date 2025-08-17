3dda7b78f6ef7a050934bced569236b3c939c4ed
MANG369 <nievesgarciamarcoantonio@gmail.com>
2025-08-17 14:10:33 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'terra-nova-cache-v3'; // Incrementa la versión si haces cambios
+const urlsToCache = [ '/', '/index.html', '/style.css', '/game.js', '/globe.png' ];
+
+self.addEventListener('install', e => e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))
+));
+self.addEventListener('fetch', e => e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+));
